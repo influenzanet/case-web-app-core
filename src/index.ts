575bead8abc4a30acc2db4fe5120faa4e625e19e
phev8 <phev8@users.noreply.github.com>
2021-12-19 16:38:20 +0100

    add export for study api

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import AppCore from './AppCore';
 import { initI18n } from './i18n';
 import store from './store/store';
+import * as studyAPI from './api/studyAPI';
 
 export {
   AppCore,
   initI18n,
   store,
+  studyAPI,
 }
