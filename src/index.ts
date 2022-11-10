e309d709c144efb989ebbe6a922f2e6fb15b304b
Clément Turbelin <clement.turbelin@gmail.com>
2022-11-10 17:51:08 +0100

    export user actions

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as studyAPI from './api/studyAPI';
 import * as userAPI from './api/userAPI';
 import { dialogActions } from './store/dialogSlice';
 import { appActions } from './store/appSlice';
+import { userActions } from './store/userSlice';
 import { useAuthTokenCheck } from "./hooks/useAuthTokenCheck";
 import PreventAccidentalNavigationPrompt from './components/misc/PreventAccidentalNavigationPrompt';
 import InternalNavigator from './components/misc/InternalNavigator';
@@ -12,6 +13,7 @@ import InternalNavigator from './components/misc/InternalNavigator';
 const coreReduxActions = {
   appActions,
   dialogActions,
+  userActions
 }
 
 export {
