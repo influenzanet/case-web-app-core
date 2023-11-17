82c943fd48038654a402880a2d0865b99e8efe1d
gf-isi <104016673+gf-isi@users.noreply.github.com>
2023-11-17 10:36:59 +0100

    SurveyList and default study entry rework
    The SurveysList is not in charge anymore
    to invite user to the default study.

---
+++ src/components/study/DefaultStudiesManager.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../store/rootReducer";
+import { initializeStudiesForUser } from "../../thunks/userThunks";
+import { Action, ThunkDispatch, unwrapResult } from "@reduxjs/toolkit";
+import {
+  enterStudy,
+  initializeActiveSurveys,
+  initializeDefaultStudies,
+} from "../../thunks/studiesThunks";
+
+const DefaultStudiesManager: React.FC = () => {
+  const dispatch = useDispatch<ThunkDispatch<RootState, void, Action>>();
+  const currentUser = useSelector((state: RootState) => state.user.currentUser);
+
+  useEffect(() => {
+    const initialize = async () => {
+      if (!currentUser.id) {
+        return;
+      }
+
+      try {
+        const defaultStudies = unwrapResult(
+          await dispatch(initializeDefaultStudies())
+        );
+        const profilesStudiesMap = unwrapResult(
+          await dispatch(initializeStudiesForUser())
+        );
+
+        /**
+         * Backward compatibility with the functionality once present
+         * in the SurveyList component
+         *
+         * We check whether all the default studies are assigned
+         * to all profiles and if not, we try entering the study
+         */
+
+        Object.keys(profilesStudiesMap).forEach((profileId) => {
+          const profileStudies = profilesStudiesMap[profileId];
+
+          if (!profileStudies) {
+            return;
+          }
+
+          const missingDefaultStudies = defaultStudies.filter(
+            (study) => !profileStudies.includes(study)
+          );
+
+          if (missingDefaultStudies.length > 0) {
+            missingDefaultStudies.forEach((studyKey) => {
+              dispatch(enterStudy({ profileId, studyKey }));
+            });
+          }
+        });
+
+        await dispatch(initializeActiveSurveys());
+      } catch {
+        /* empty */
+      }
+    };
+
+    initialize();
+  }, [currentUser.id, dispatch]);
+
+  return <></>;
+};
+
+export default DefaultStudiesManager;
---
+++ src/store/studiesSlice.ts
@@ -0,0 +1,42 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { initializeDefaultStudies } from "../thunks/studiesThunks";
+import { SurveyInfo } from "../api/types/studyAPI";
+import { isEqual } from "lodash-es";
+import { userActions } from "./userSlice";
+
+export interface StudiesState {
+  defaultStudies: string[];
+  activeSurveyInfos: SurveyInfo[];
+}
+
+export const initialState: StudiesState = {
+  defaultStudies: [],
+  activeSurveyInfos: [],
+};
+
+const studiesSlice = createSlice({
+  name: "studies",
+  initialState: initialState,
+  reducers: {
+    initializeActiveSurveyInfos: (
+      state,
+      action: PayloadAction<SurveyInfo[]>
+    ) => {
+      if (!isEqual(state.activeSurveyInfos, action.payload)) {
+        state.activeSurveyInfos = action.payload;
+      }
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(userActions.reset, () => {
+      return initialState;
+    });
+    builder.addCase(initializeDefaultStudies.fulfilled, (state, action) => {
+      state.defaultStudies = action.payload;
+    });
+  },
+});
+
+export const studiesActions = studiesSlice.actions;
+
+export default studiesSlice.reducer;
